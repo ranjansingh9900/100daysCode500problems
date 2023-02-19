afe76b56ee4086f0e7efa36f3aeee6dfb23b100f
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-02-19 20:39:05 +0530

    Update 03_String_GFG_all_question.cpp

--- 03_String_GFG_all_question.cpp
+++ 03_String_GFG_all_question.cpp
@@ -787,6 +787,246 @@ int main()
 
 
 
+// 17...Word break Problem[ Very Imp]
+
+#include <iostream>
+using namespace std;
+
+
+
+int dictionaryContains(string word)
+{
+	string dictionary[] = {"mobile","samsung","sam","sung",
+							"man","mango","icecream","and",
+							"go","i","like","ice","cream"};
+	int size = sizeof(dictionary)/sizeof(dictionary[0]);
+	for (int i = 0; i < size; i++)
+		if (dictionary[i].compare(word) == 0)
+		return true;
+	return false;
+}
+
+bool wordBreak(string str)
+{
+	int size = str.size();
+
+	// Base case
+	if (size == 0) return true;
+
+	// Try all prefixes of lengths from 1 to size
+	for (int i=1; i<=size; i++)
+	{
+		// The parameter for dictionaryContains is
+		// str.substr(0, i) which is prefix (of input
+		// string) of length 'i'. We first check whether
+		// current prefix is in dictionary. Then we
+		// recursively check for remaining string
+		// str.substr(i, size-i) which is suffix of
+		// length size-i
+		if (dictionaryContains( str.substr(0, i) ) &&
+			wordBreak( str.substr(i, size-i) ))
+			return true;
+	}
+
+	// If we have tried all prefixes and
+	// none of them worked
+	return false;
+}
+
+
+int main()
+{
+	wordBreak("ilikesamsung")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("iiiiiiii")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("ilikelikeimangoiii")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("samsungandmango")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("samsungandmangok")? cout <<"Yes\n": cout << "No\n";
+	return 0;
+}
+
+
+
+// 18...Rabin Karp Algo
+/* Following program is a C++ implementation of Rabin Karp
+Algorithm given in the CLRS book */
+#include <bits/stdc++.h>
+using namespace std;
+
+// d is the number of characters in the input alphabet
+#define d 256
+
+/* pat -> pattern
+	txt -> text
+	q -> A prime number
+*/
+void search(char pat[], char txt[], int q)
+{
+	int M = strlen(pat);
+	int N = strlen(txt);
+	int i, j;
+	int p = 0; // hash value for pattern
+	int t = 0; // hash value for txt
+	int h = 1;
+
+	// The value of h would be "pow(d, M-1)%q"
+	for (i = 0; i < M - 1; i++)
+		h = (h * d) % q;
+
+	// Calculate the hash value of pattern and first
+	// window of text
+	for (i = 0; i < M; i++) {
+		p = (d * p + pat[i]) % q;
+		t = (d * t + txt[i]) % q;
+	}
+
+	// Slide the pattern over text one by one
+	for (i = 0; i <= N - M; i++) {
+
+		// Check the hash values of current window of text
+		// and pattern. If the hash values match then only
+		// check for characters one by one
+		if (p == t) {
+			/* Check for characters one by one */
+			for (j = 0; j < M; j++) {
+				if (txt[i + j] != pat[j]) {
+					break;
+				}
+			}
+
+			// if p == t and pat[0...M-1] = txt[i, i+1,
+			// ...i+M-1]
+
+			if (j == M)
+				cout << "Pattern found at index " << i
+					<< endl;
+		}
+
+		// Calculate hash value for next window of text:
+		// Remove leading digit, add trailing digit
+		if (i < N - M) {
+			t = (d * (t - txt[i] * h) + txt[i + M]) % q;
+
+			// We might get negative value of t, converting
+			// it to positive
+			if (t < 0)
+				t = (t + q);
+		}
+	}
+}
+
+/* Driver code */
+int main()
+{
+	char txt[] = "GEEKS FOR GEEKS";
+	char pat[] = "GEEK";
+
+	// we mod to avoid overflowing of value but we should
+	// take as big q as possible to avoid the collison
+	int q = INT_MAX;
+
+	// Function Call
+	search(pat, txt, q);
+	return 0;
+}
+
+
+
+// 19...KMP Algo
+// C++ program for implementation of KMP pattern searching
+// algorithm
+#include <bits/stdc++.h>
+
+void computeLPSArray(char* pat, int M, int* lps);
+
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(char* pat, char* txt)
+{
+	int M = strlen(pat);
+	int N = strlen(txt);
+
+	// create lps[] that will hold the longest prefix suffix
+	// values for pattern
+	int lps[M];
+
+	// Preprocess the pattern (calculate lps[] array)
+	computeLPSArray(pat, M, lps);
+
+	int i = 0; // index for txt[]
+	int j = 0; // index for pat[]
+	while ((N - i) >= (M - j)) {
+		if (pat[j] == txt[i]) {
+			j++;
+			i++;
+		}
+
+		if (j == M) {
+			printf("Found pattern at index %d ", i - j);
+			j = lps[j - 1];
+		}
+
+		// mismatch after j matches
+		else if (i < N && pat[j] != txt[i]) {
+			// Do not match lps[0..lps[j-1]] characters,
+			// they will match anyway
+			if (j != 0)
+				j = lps[j - 1];
+			else
+				i = i + 1;
+		}
+	}
+}
+
+// Fills lps[] for given pattern pat[0..M-1]
+void computeLPSArray(char* pat, int M, int* lps)
+{
+	// length of the previous longest prefix suffix
+	int len = 0;
+
+	lps[0] = 0; // lps[0] is always 0
+
+	// the loop calculates lps[i] for i = 1 to M-1
+	int i = 1;
+	while (i < M) {
+		if (pat[i] == pat[len]) {
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else // (pat[i] != pat[len])
+		{
+			// This is tricky. Consider the example.
+			// AAACAAAA and i = 7. The idea is similar
+			// to search step.
+			if (len != 0) {
+				len = lps[len - 1];
+
+				// Also, note that we do not increment
+				// i here
+			}
+			else // if (len == 0)
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
+}
+
+// Driver code
+int main()
+{
+	char txt[] = "ABABDABACDABABCABAB";
+	char pat[] = "ABABCABAB";
+	KMPSearch(pat, txt);
+	return 0;
+}
+
+
+
+
+
+
 
 
 
