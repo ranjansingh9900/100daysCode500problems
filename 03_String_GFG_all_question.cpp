7755553371e20b4a2190693e33a1c66b15010cd0
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-02-19 20:24:31 +0530

    Update 03_String_GFG_all_question.cpp

--- 03_String_GFG_all_question.cpp
+++ 03_String_GFG_all_question.cpp
@@ -704,6 +704,93 @@ int main()
 
 
 
+// 15...Find next greater number with same set of digits. [Very Very IMP]
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find the next permutation
+void nextPermutation(vector<int>& arr)
+{
+	next_permutation(arr.begin(),arr.end());
+}
+
+// Driver code
+int main()
+{
+
+	// Given input array
+	vector<int> arr = { 1, 2, 3, 6, 5, 4 };
+
+	// Function call
+	nextPermutation(arr);
+
+	// Printing the answer
+	for (auto i : arr) {
+		cout << i << " ";
+	}
+
+	return 0;
+}
+
+
+
+// 16...Balanced Parenthesis problem.[Imp]
+// C++ program to check for balanced brackets.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check if brackets are balanced
+bool areBracketsBalanced(string expr)
+{
+	// Declare a stack to hold the previous brackets.
+	stack<char> temp;
+	for (int i = 0; i < expr.length(); i++) {
+		if (temp.empty()) {
+			
+			// If the stack is empty
+			// just push the current bracket
+			temp.push(expr[i]);
+		}
+		else if ((temp.top() == '(' && expr[i] == ')')
+				|| (temp.top() == '{' && expr[i] == '}')
+				|| (temp.top() == '[' && expr[i] == ']')) {
+			
+			// If we found any complete pair of bracket
+			// then pop
+			temp.pop();
+		}
+		else {
+			temp.push(expr[i]);
+		}
+	}
+	if (temp.empty()) {
+		
+		// If stack is empty return true
+		return true;
+	}
+	return false;
+}
+
+// Driver code
+int main()
+{
+	string expr = "{()}[]";
+
+	// Function call
+	if (areBracketsBalanced(expr))
+		cout << "Balanced";
+	else
+		cout << "Not Balanced";
+	return 0;
+}
+
+
+
+
+
+
+
 
 
 
