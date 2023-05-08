7d4732e7134cce702cb4316851be2eda476dafdf
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-05-08 23:37:10 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -68,3 +68,72 @@ int formingMagicSquare(vector<vector<int>> s) {
     // Return the minimum cost
     return *min_element(costs.begin(), costs.end());
 }
+dshffsjfhsjf
+
+fgg
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> q1, q2;
+        int n = senate.length();
+        for(int i = 0; i<n; i++)
+            (senate[i] == 'R')?q1.push(i):q2.push(i);
+        while(!q1.empty() && !q2.empty()){
+            int r_index = q1.front(), d_index = q2.front();
+            q1.pop(), q2.pop();
+            (r_index < d_index)?q1.push(r_index + n):q2.push(d_index + n);
+        }
+        return (q1.size() > q2.size())? "Radiant" : "Dire";
+    }
+};
+
+
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        left, right = 0, len(nums) - 1
+        count = 0
+        mod = 10 ** 9 + 7
+        
+        while left <= right:
+            if nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                count += pow(2, right - left, mod)
+                left += 1
+        
+        return count % mod
+
+\
+
+
+
+int formingMagicSquare(vector<vector<int>> s) {
+    // List of all possible 3x3 magic squares
+    vector<vector<vector<int>>> magicSquares = {
+        {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+        {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+        {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+        {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+        {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+        {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+        {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+        {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}
+    };
+    
+    // Calculate the cost of converting s to each magic square
+    vector<int> costs;
+    for (auto magicSquare : magicSquares) {
+        int cost = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cost += abs(s[i][j] - magicSquare[i][j]);
+            }
+        }
+        costs.push_back(cost);
+    }
+    
+    // Return the minimum cost
+    return *min_element(costs.begin(), costs.end());
+}
