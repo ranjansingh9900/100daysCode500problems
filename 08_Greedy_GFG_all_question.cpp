3ccb80fc4887c499d87680b7d84153237e1998a5
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-05-16 23:27:29 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -434,4 +434,31 @@ public:
 
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next)return head;
+        if(head!=NULL && head->next!=NULL){
+            swap(head->val,head->next->val);
+            swapPairs(head->next->next);
+        }
+        return head;
+    }
+    // return head;
+};
+
+
+
+
+
 
