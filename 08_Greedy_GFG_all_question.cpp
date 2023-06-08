6c657fa9347d344c20ceee6c480cc35244c3af90
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-06-08 06:49:04 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -1229,6 +1229,29 @@ public:
 
 
 
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+       int i=0;
+       int j=grid[0].size()-1;
+       int count=0;
+       while(i<grid.size()&&j>=0)
+       {
+           if(grid[i][j]<0)
+           {
+               count+=(grid.size()-i);
+               j--;
+           }
+           else if(grid[i][j]>=0)
+           {
+               i++;
+           }
+       }
+       return count;
+    }
+};
+
+
 
 
 
