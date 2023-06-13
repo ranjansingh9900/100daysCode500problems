14803d54b7bd2107c93194c445cd66e269c7edb8
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-06-13 22:57:07 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -22,6 +22,26 @@ public:
 
 
 
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& v) {
+        int n=v.size();
+        int r=0;
+        vector<vector<int>>c=v;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++)swap(v[i][j],v[j][i]);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(v[i]==c[j])r++;
+            }
+        }
+        return r;
+    }
+};
+
+
+
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
         nums.sort()
