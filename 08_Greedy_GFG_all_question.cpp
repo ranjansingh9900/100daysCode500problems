14830e2001524c95bce3277cf79c64ecbf10256e
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-07-08 22:46:29 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -3,6 +3,28 @@ dshffsjfhsjf
 
 fgg
 
+
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        vector<long long> store;
+        for(int i=1;i<weights.size();i++){
+            store.push_back(weights[i]+weights[i-1]);
+        }
+        sort(store.begin(),store.end());
+        int n=store.size();
+        long long mini=0;
+        for(int i=0;i<k-1;i++){
+            mini+= store[i];
+        }
+
+        long long maxi=0;
+        for(int i=0;i<k-1;i++){
+            maxi+= store[n-1-i];
+        }
+        return maxi - mini;
+    }
+};
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
