28f1221b26537f2d99a9ac6f2bbd80bcd27eaf2e
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-08-07 23:49:10 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -21,6 +21,19 @@ public:
 };
 class Solution {
 public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r=0,c=matrix[0].size()-1;
+        while(r<matrix.size() && c>=0){
+            if(matrix[r][c]==target)
+                return true;
+            else if(matrix[r][c]<target)r++;
+            else c--;
+        }
+        return false;
+    }
+};
+class Solution {
+public:
     vector<vector<int>> ans;
     void dfs(int i , int n , int k , vector<int>& temp){
         if(k == 0){
