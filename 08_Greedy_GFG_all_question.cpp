96e2fb06fa542152d47152a4a1a915955a035309
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-08-27 23:25:01 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -2890,5 +2890,28 @@ double dfs(int i,int j,int k, int n)
     }
 };
 
+class Solution {
+public:
+    bool canCross(std::vector<int>& stones) {
+        int n = stones.size();
 
+        std::unordered_map<int, std::unordered_set<int>> dp;
+        for (int stone : stones) {
+            dp[stone] = std::unordered_set<int>();
+        }
+        dp[0].insert(0);
+
+        for (int i = 0; i < n; i++) {
+            for (int k : dp[stones[i]]) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step != 0 && dp.count(stones[i] + step)) {
+                        dp[stones[i] + step].insert(step);
+                    }
+                }
+            }
+        }
+        
+        return !dp[stones.back()].empty();
+    }
+};
 
