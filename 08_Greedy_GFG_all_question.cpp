b6fd05d0381ed53b73dd0bb52c0c90f29f98ea7f
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-08-28 23:44:44 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -2915,3 +2915,52 @@ public:
     }
 };
 
+class MyStack {
+public:
+    queue<int>q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        int s = q.size();
+        q.push(x);
+        for(int i=0; i<s; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        if(q.size() == 0)
+        {
+            return -1;
+        }
+        
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        if(q.size() == 0)
+        {
+            return -1;
+        }
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.size() == 0;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
