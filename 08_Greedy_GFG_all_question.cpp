ac929620b8505f899b04c05a36fa627cb1778748
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-09-12 19:21:45 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -3258,3 +3258,28 @@ public:
         return v;
     }
 };
+
+class Solution {
+public:
+    int minDeletions(string s) {
+        unordered_map<char, int> chars;
+        for (char c : s) {
+            chars[c]++;
+        }
+
+        unordered_set<int> freqSet;
+        int count = 0;
+
+        for (const auto& pair : chars) {
+            int freq = pair.second;
+            while (freq > 0 && freqSet.count(freq)) {
+                freq--;
+                count++;
+            }
+            freqSet.insert(freq);
+        }
+
+        return count;        
+    }
+};
+
