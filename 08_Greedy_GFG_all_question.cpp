da7d734c9cfb4fe6cca4a3220a3ff0f58fa89c4d
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-09-19 20:47:32 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -20,6 +20,27 @@ public:
     }
 };
 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int, int> m;
+        int duplicate = 0;
+        for(auto i= 0; i<nums.size(); ++i)
+        ++m[nums[i]];
+        
+        for(auto i : m)
+        {
+            if(i.second > 1)
+            {
+                duplicate = i.first;
+                break;
+            }
+        }
+        return duplicate;
+    }
+
+};
+
 
 /**
  * Definition for singly-linked list.
