39894f2f7d2b6e1c39807364094231196bd259f0
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-09-29 23:10:07 +0530

    Update 08_Greedy_GFG_all_question.cpp

--- 08_Greedy_GFG_all_question.cpp
+++ 08_Greedy_GFG_all_question.cpp
@@ -3810,3 +3810,17 @@ public:
         return nums;
     }
 };
+
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int direction = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(direction > 0 && nums[i] < nums[i-1]) return false;
+            if(direction < 0 && nums[i] > nums[i-1]) return false;
+            if(direction == 0) direction = nums[i] - nums[i-1];
+        }
+        return true;
+    }
+};
